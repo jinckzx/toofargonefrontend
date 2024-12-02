ea75c2a7c4cfa9d929a117772069ada477f817cf
Anmol <89922277+jinckzx@users.noreply.github.com>
2024-12-02 22:54:13 +0530

    Create AnnouncementBanner.tsx

---
+++ src/components/AnnouncementBanner.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+const announcements = [
+  "FREE DOMESTIC SHIPPING",
+  "USE CODE \"TFG200\" ON ORDERS ABOVE 2500"
+];
+
+export default function AnnouncementBanner() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % announcements.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="bg-black text-white py-0.5">
+      <div className="relative overflow-hidden h-5">
+        <div
+          className="whitespace-nowrap transition-transform duration-500 ease-in-out absolute w-full text-center"
+          style={{
+            transform: `translateX(${-currentIndex * 100}%)`,
+          }}
+        >
+          {announcements.map((text, index) => (
+            <span
+              key={index}
+              className="inline-block w-full text-xs font-medium tracking-wide"
+            >
+              {text}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
