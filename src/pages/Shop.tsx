82f0139f01f3b8ce0b7803098baacdb5450e36c2
Anmol <89922277+jinckzx@users.noreply.github.com>
2024-12-12 16:42:27 +0530

    Update Shop.tsx

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -390,7 +390,7 @@ export default function Shop() {
 
           {/* Product Grid */}
           <div className="flex-1">
-            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 ">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-1 ">
               {products.map((product) => (
                 <motion.div
                   key={product.productId}
